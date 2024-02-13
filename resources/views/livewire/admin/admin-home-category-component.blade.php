522865620a82c7947d822150f9b76df9c0ff69ef
Carlos <carolomon2@gmail.com>
2024-02-13 20:35:03 +0100

    home categories dynamic

---
+++ resources/views/livewire/admin/admin-home-category-component.blade.php
@@ -0,0 +1,60 @@
+<div>
+    <div class="container" style="padding: 30px 0;">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <div class="row">
+                            <div class="col-md-6">
+                                Manage Home Categories
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="panel-body">
+                        @if(Session::has('message'))
+                            <div class="alert alert-success" role="alert">{{Session::get('message')}}</div>
+                        @endif
+                        <form class="form-horizontal" wire:submit.prevent="updateHomeCategory">
+                            <div class="form-group">
+                                <label class="col-md-4 control-label" for="">Choose Categories</label>
+                                <div class="col-md-4" wire:ignore>
+                                    <select class="form-control sel_categories" name="categories[]" multiple="multiple" wire:model="selected_categories">
+                                        @foreach ($categories as $category)
+                                            <option value="{{$category->id}}">{{$category->name}}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="col-md-4 control-label" for="">Number of Products</label>
+                                <div class="col-md-4">
+                                    <input type="text" class="form-control input-md" wire:model="no_of_products">
+                                </div>
+                            </div>
+                            
+                            <div class="form-group">
+                                <label class="col-md-4 control-label" for=""></label>
+                                <div class="col-md-4">
+                                    <button type="submit" class="btn btn-primary">Save</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@push('scripts')
+    <script>
+        $(document).ready(function(){
+            $('.sel_categories').select2();
+            $('.sel_categories').on('change',function(){
+                var data = $('.sel_categories').select2('val');
+                @this.set('selected_categories', data);
+            });
+        });
+    </script>
+@endpush
